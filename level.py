cb92f339dca63d615ea353c902d5c811432a3c0f
Jonnathan <stvns.j@proton.me>
2024-09-28 00:38:33 -0300

    Added Level Code

--- level.py
+++ level.py
@@ -1,10 +1,11 @@
-
+import sys
+import numpy as np
 
 
 
 class PR :
     
-    def __init__ (self, name, back_delta, front_delta, height=0):
+    def __init__ (self, name, back_delta = 0.0, front_delta = 0.0, height = 0.0):
      
         self.name = name;
         self.back_delta = back_delta;
@@ -32,7 +33,6 @@ class ControlPoint :
     def __init__ (self,
                   
                   num,
-                  prev_control_point,
                   back_delta = 0.0,
                   front_delta = 0.0,
                   
@@ -46,9 +46,13 @@ class ControlPoint :
         
         self.back_delta  = back_delta;
         self.front_delta = front_delta;
+
+        
         
         self.point_uncorrected = 0.0;
         self.instr_uncorrected = 0.0;
+        self.inter_corrected_list = []
+        self.instr_corrected = 0.0;
         
         
  
@@ -68,6 +72,12 @@ class ControlPoint :
     def __init_point_uncorrected (self, prev_control_point):
         instr = prev_control_point.instr_uncorrected
         self.point_uncorrected = instr - self.front_delta
+ 
+    def correct_instr (self, correction):
+        self.instr_corrected = self.instr_uncorrected + correction * self.num
+ 
+    def correct_inter_list (self):
+        return self.instr_corrected - self.inter_list;
     
 
 
@@ -86,6 +96,8 @@ class Segment :
         
         self.point_uncorrected = pr0.get_point_uncorrected() # Last uncorrected point.
         self.ref_height = 0
+
+        self.size = 0;
  
     def __build (self):
         
@@ -105,10 +117,16 @@ class Segment :
         point_uncorrected = instr_uncorrected - pr1.front_delta
         
         self.diff = ref_height - point
+        self.size = len(self.control_point_list)
+        
+     
+        for cp in self.control_point_list:
+            cp.correct_instr(self.diff / self.size);
+        
  
     def __correct (self):
         for cp in self.control_point_list:
-            print(cp.num * self.diff)
+            print(self.inter_corrected_list)
 
 
 
@@ -121,3 +139,35 @@ class Circuit :
 class Model :
     def __init__ (self):
         pass
+
+
+class Processor :
+    
+    def __init__ (self, num_matrix, string_matrix):
+        
+        start = 0
+        end   = 1
+        
+        for symbol in string_matrix[1:]:
+            
+            if symbol.startswith("P") or symbol.startswith("p"):
+                print(num_matrix[start:end])
+                end += 1;
+                start = end;
+                input("===================")
+            
+            else:
+                end += 1;
+
+
+
+
+filename = sys.argv[1]
+
+print(filename)
+
+string_matrix = np.genfromtxt(filename, delimiter=',', dtype=str, skip_header=0)[:,0]
+num_matrix    = np.genfromtxt(filename, delimiter=',', skip_header = 0)[: , [1,2,3,4,5]]
+
+pro = Processor(num_matrix, string_matrix)
+# string_matrix = np.genfromtxt(path1, delimiter=',', skip_header=0)
