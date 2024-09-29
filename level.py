6a18845c98e5b231b5354db0b5d2349038aa7648
Jonnathan <stvns.j@proton.me>
2024-09-29 08:17:43 -0300

    Several changes on level.py

--- level.py
+++ level.py
@@ -3,130 +3,134 @@ import numpy as np
 
 
 
-class PR :
-    
-    def __init__ (self, name, back_delta = 0.0, front_delta = 0.0, height = 0.0):
-     
-        self.name = name;
-        self.back_delta = back_delta;
-        self.front_delta = front_delta;
-        self.height = height
- 
-    def get_point_uncorrected (self) :
-        return self.height
-    
-    def get_instr_uncorrected (self) :
-        return self.height + self.back_delta;
-  
-    def get_back_delta (self):
-        return self.back_delta;
- 
-    def get_front_delta (self):
-        return self.front_delta;
- 
-    
-    
 
 
-class ControlPoint :
-    
-    def __init__ (self,
-                  
-                  num,
-                  back_delta = 0.0,
-                  front_delta = 0.0,
-                  
-                  dm_list = [],
-                  inter_list = []):
-        
-        self.num         = num;
-        
-        self.dm_list     = dm_list;
-        self.inter_list  = inter_list;
-        
-        self.back_delta  = back_delta;
-        self.front_delta = front_delta;
 
+class Point :
+    
+    def __init__ (
+            self,
+            num,
+            start = False,
+            point_uncorrected = 0.0,
+            back_delta = 0.0,
+            front_delta = 0.0,
+            dm = [],
+            intermediate = []):
         
+        self.num                     = num
+        self.start                   = start
         
-        self.point_uncorrected = 0.0;
-        self.instr_uncorrected = 0.0;
-        self.inter_corrected_list = []
-        self.instr_corrected = 0.0;
+        self.dm                      = dm
+        self.intermediate            = intermediate
+        
+        self.back_delta              = back_delta
+        self.front_delta             = front_delta
+        
+        self.point_uncorrected       = point_uncorrected
+        self.instr_uncorrected       = 0.0
+        
+        self.point_corrected  = []
+        self.instr_corrected  = 0.0
         
         
  
-    def build (self, prev):
+    def build (self, prev = None):
         """initializes point_uncorrected and instr_uncorrected
         interacting with previous control point."""
-        self.point_uncorrected = prev_control_point.get_point_uncorrected()
-        self.instr_uncorrected = self.point_uncorrected + self.back_delta
+        if not self.start:
+            self.__init_point_uncorrected (prev)
+        self.__init_instr_uncorrected ()
         
     
-    def get_point_uncorrected (self):
-        return self.point_uncorrected
-    
-    def get_instr_uncorrected (self):
-        return self.instr_uncorrected
-    
-    def __init_point_uncorrected (self, prev_control_point):
-        instr = prev_control_point.instr_uncorrected
+    def __init_point_uncorrected (self, prev):
+        
+        instr = prev.instr_uncorrected
         self.point_uncorrected = instr - self.front_delta
+        return
+    
+    def __init_instr_uncorrected (self) :
+        self.instr_uncorrected = self.point_uncorrected + self.back_delta
+        
  
+    
     def correct_instr (self, correction):
         self.instr_corrected = self.instr_uncorrected + correction * self.num
  
-    def correct_inter_list (self):
-        return self.instr_corrected - self.inter_list;
+ 
+    def correct_point (self):
+        self.point_corrected =  self.instr_corrected - self.intermediate;
     
+    
+    
+    
+    
+    
+    
+    ###########
+    # GETTERS #
+    ###########
+    def get_point_uncorrected (self):
+        return self.point_uncorrected
+    def get_instr_uncorrected (self):
+        return self.instr_uncorrected
+    
+    #########
+    # PRINT #
+    #########
+    def __str__(self):
+        return f'DM = {self.dm}\nuncorrected point = {self.point_uncorrected}'
+      
 
 
 
 
 class Segment :
     
-    def __init__ (self, pr0, pr1, ref_height, cplst=[]):
+    def __init__ (self, pr0, pr1, first_height, last_height, cplst=[]):
         
-        self.pr0 = pr0;
-        self.pr1 = pr1;
+        self.pr0 = pr0 # string name of pr0
+        self.pr1 = pr1 # string name of pr1
         
-        self.control_point_list = cplst;
+        self.control_point_list = cplst
         
-        self.diff = 0;
+        self.diff = 0
+     
+        self.first_height = 0
+        self.last_height  = 0
         
-        self.point_uncorrected = pr0.get_point_uncorrected() # Last uncorrected point.
-        self.ref_height = 0
-
-        self.size = 0;
+        self.point_uncorrected = last_height
+        
+        self.ref_height = last_height
+        
+        self.size = 0
+        
+        self.__build()
  
     def __build (self):
-        
-        instr_uncorrected = pr0.get_instr_uncorrected()
         
         for i , cp in enumerate(self.control_point_list) :
             
             if i == 0:
-                cp.build(pr0)
-                instr_uncorrected = cp.get_point_uncorrected()
+                cp.build()
                 continue
             
             cp.build(self.control_point_list[i-1])
-            instr_uncorrected = cp.get_instr_uncorrected()
-         
         
-        point_uncorrected = instr_uncorrected - pr1.front_delta
+        point_uncorrected = self.control_point_list[-1].get_point_uncorrected()
         
-        self.diff = ref_height - point
-        self.size = len(self.control_point_list)
+        self.diff = self.ref_height - point_uncorrected
+        self.size = len(self.control_point_list) - 1
         
      
         for cp in self.control_point_list:
             cp.correct_instr(self.diff / self.size);
+            cp.correct_point()
         
  
-    def __correct (self):
-        for cp in self.control_point_list:
-            print(self.inter_corrected_list)
+
+    def __str__ (self) :
+        return f'pr0 = {self.pr0}\npr1 = {self.pr1}\npoint corrected = {self.control_point_list}'
 
 
 
@@ -141,33 +145,125 @@ class Model :
         pass
 
 
-class Processor :
+
+def parse_circuit (circuit_matrix, height_matrix):
+    start = 0
+    end   = 1
+    height_dict = dict(height_matrix)
+    segment_list = []
     
-    def __init__ (self, num_matrix, string_matrix):
-        
-        start = 0
-        end   = 1
-        
-        for symbol in string_matrix[1:]:
+    for i , row in enumerate(circuit_matrix):
+        if row[0] != "" :
             
-            if symbol.startswith("P") or symbol.startswith("p"):
-                print(num_matrix[start:end])
-                end += 1;
-                start = end;
-                input("===================")
+            start = i
+           
+        if row[2] != "":
             
-            else:
-                end += 1;
+            end = i
+         
+            pr0 = circuit_matrix[start][0]
+            pr1 = circuit_matrix[end][2]
+         
+            h0  = float(height_dict[pr0])
+            h1  = float(height_dict[pr1])
+           
+            seg = parse_segment(circuit_matrix[start:end+1], pr0, pr1, h0, h1)
+            segment_list.append(seg)
+            
+    return segment_list
+
+
+def parse_segment (string_matrix, pr0, pr1, h0, h1):
+    
+    POINT_NUM = 0
+    START = 0
+    END   = 0
+    N     = len(string_matrix)
+ 
+    point_list = []
+    
+    for i, row in enumerate(string_matrix[1:]):
+      
+        if row[1] == "" and  string_matrix[i][1] == "":
+            END = i+1
+            
+            first = True if POINT_NUM == 0 else False
+            point = parse_point(POINT_NUM, first, h0, string_matrix[START:END])
+            point_list.append(point)
+            POINT_NUM += 1
+            
+            START = i+1
+            
+            continue
+        
+        if row[1] == "":
+            END = i+1
+            
+            first = True if POINT_NUM == 0 else False
+            point = parse_point(POINT_NUM, first, h0, string_matrix[START:END])
+            point_list.append(point)
+            POINT_NUM += 1
+            
+            START = i+1
+            continue
+        
+        if i == N-2:
+            END = i+1
+            first = True if POINT_NUM == 0 else False
+            point = parse_point(POINT_NUM, first, h0, string_matrix[START:END])
+            print("=================\n\n")
+            print(point)
+            print("=================\n\n")
+            point_list.append(point)
+            continue
+
+    for point in point_list:
+        print(point)
+ 
+    return Segment(pr0, pr1, h0, h1, point_list)
+
+
+def parse_point (num, start, h0, string_matrix):
+ 
+    back_delta  = float(string_matrix[0][3]) if string_matrix[0][3] != "" else 0.0
+    front_delta = float(string_matrix[0][5]) if string_matrix[0][5] != "" else 0.0
+    point_uncorrected = float(h0) if start else 0.0
+ 
+    dm = []
+    im = []
+ 
+    if len(string_matrix) > 0 :
+        #dm = np.where(string_matrix[1:,1] == "", "0.0", string_matrix[1:,1]).astype(float)
+        dm = string_matrix[1:,1]
+        im = np.where(string_matrix[1:,4] == "", "0.0", string_matrix[1:,4]).astype(float)
+    
+    
+    point = Point(
+        num,
+        start=start,
+        point_uncorrected=point_uncorrected,
+        back_delta = back_delta,
+        front_delta = front_delta,
+        dm = dm,
+        intermediate = im
+    )
+    return point
 
 
 
+if __name__ == "__main__":
+ 
+    filename1 = sys.argv[1]
+    filename2 = sys.argv[2]
+ 
+    print(filename1)
+ 
+    # string_matrix = np.genfromtxt(filename, delimiter=',', dtype=str, skip_header=0)[:,0]
+    string_matrix = np.genfromtxt(filename1, delimiter=',', dtype=str, skip_header=0)
+    height_matrix = np.genfromtxt(filename2, delimiter=',', dtype=str, skip_header=0)
+ 
+    pro = parse_circuit(string_matrix, height_matrix)
 
-filename = sys.argv[1]
-
-print(filename)
-
-string_matrix = np.genfromtxt(filename, delimiter=',', dtype=str, skip_header=0)[:,0]
-num_matrix    = np.genfromtxt(filename, delimiter=',', skip_header = 0)[: , [1,2,3,4,5]]
-
-pro = Processor(num_matrix, string_matrix)
-# string_matrix = np.genfromtxt(path1, delimiter=',', skip_header=0)
+    for seg in pro:
+        for p in seg.control_point_list:
+            print(p)
