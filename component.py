29642ef2a1059103a9ec17e75c40f1087f150722
Jonnathan <stvns.j@proton.me>
2024-10-03 02:16:10 -0300

    New widget for the "Generate" Buttons

--- component.py
+++ component.py
@@ -17,6 +17,37 @@ from tkinter import filedialog
 # which is used to save file in any extension 
 from tkinter.filedialog import asksaveasfile
 
+class ButtonFrame(tk.Frame):
+        
+    def __init__ (self, parent, title="Load File", button_params = []):
+        
+        super().__init__(parent,pady=10,padx=10,bd=3,relief="groove")
+        self.row = 0
+        self.pack()
+        self.insert_title(title)
+        self.frame_grid = tk.Frame(self)
+        self.frame_grid.pack()
+        for param in button_params:
+            self.insert_button(param["label"],param["command"])
+            self.row += 1
+ 
+    def insert_title(self, title):
+        label_title = tk.Label(self, text = title,pady=10,font='Helvetica 10 bold')
+        label_title.pack()
+ 
+    def insert_button (self, label, command=lambda:print("ButtonFrame button")):
+        
+        label     = tk.Label(self.frame_grid,  text=label, font='Helvetica 10 italic', width=26, anchor="w")
+        button    = tk.Button(self.frame_grid, text="Generar", command=command)
+        
+        label.grid(row=self.row,column=0)
+        button.grid(row=self.row,column=1)
+        
+    
+    def load_file_command(self,stringvar) :
+        pass
+    
+    
 
 class LoadFileFrame(tk.Frame):
     
@@ -38,8 +69,8 @@ class LoadFileFrame(tk.Frame):
  
     def insert_button (self, label, stringvar):
         button    = tk.Button(self.frame_grid, text="Cargar", command=lambda:self.load_file_command(stringvar))
-        label     = tk.Label(self.frame_grid,  text=label, font='Helvetica 10 bold')
-        filelabel = tk.Label(self.frame_grid, textvariable=stringvar)
+        label     = tk.Label(self.frame_grid,  text=label, font='Helvetica 10 italic', width=26, anchor="w")
+        filelabel = tk.Label(self.frame_grid, textvariable=stringvar, bg='white', bd=1, relief="solid", width=50,anchor="w")
         
         button.grid(row=self.row,column = 0)
         label.grid(row=self.row,column=1)
