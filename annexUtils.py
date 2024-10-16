55fc78eadb6476cf47fd1eff32b0b4c020ac5a2f
Jonnathan <stvns.j@proton.me>
2024-10-16 06:48:52 -0300

    Annex Utils

---
+++ annexUtils.py
@@ -0,0 +1,84 @@
+
+
+INCH_COL = 10
+INCH_ROW = 72
+OFFSET   = 38
+PAGEBREAKS = []
+
+
+class Writer :
+    def __init__ (self,workbook,worksheet):
+        self.workbook = workbook
+        self.worksheet = worksheet
+    
+    def set_worksheet (self,ws):
+        self.worksheet = ws
+    
+    def merge (self,ran,dat,*dic):
+        
+        
+        CELL_FORMAT = {}
+        for f in dic :
+            CELL_FORMAT = CELL_FORMAT | f
+            
+        self.worksheet.merge_range(
+            ran,
+            dat,
+            self.workbook.add_format(CELL_FORMAT)
+        )
+    
+    def write (self,cell,dat,*dic):
+        
+        CELL_FORMAT = {}
+        for f in dic :
+            CELL_FORMAT = CELL_FORMAT | f
+            
+        self.worksheet.write(
+            cell,
+            dat,
+            self.workbook.add_format(CELL_FORMAT)
+        )
+
+
+class Format :
+    
+    def SIZE(n):
+        return {"font_size":n}
+    
+    BOLD = {"bold":True}
+    ITALIC = {"italic":True}
+    
+    TOP  = {"top" :1}
+    BTOP = {"top" :1}
+    BBOTTOM = {"bottom" :1}
+    BOTTOM = {"bottom":1}
+    BLEFT = {"left": 1}
+    BRIGHT = {"right": 1}
+    
+    BORDER = {"border":1}
+    
+    LEFT = {"align":"left"}
+    RIGHT = {"align":"right"}
+    
+    CENTER = {"align":"center"}
+    VCENTER = {"valign":"vcenter"}
+
+
+
+
+class Formatter :
+    
+    def __init__(self, ws):
+        self.ws = ws
+        
+    
+    def set_cols(self,dic):
+        for col in dic:
+            size = dic[col]
+            self.ws.set_column(col,col,size*INCH_COL) 
+            
+    
+    def set_rows(self,dic):
+        for row in dic:
+            size = dic[row]
+            self.ws.set_row(row,size*INCH_ROW)
